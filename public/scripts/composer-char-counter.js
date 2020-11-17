0ca789f392ca1644a4828026c4d68c0299e46a01
Benjamin Lee <benjaminjslee@gmail.com>
2020-11-17 22:02:55 +0000

    Add js for char counter; add tweet styles
    The form for tweeting now has a char counter.
    Minor changes to tweet form and styles.
    Tweets now have styles and general mark-up.
    Modified conflictive nav styles.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+const TWEET_LENGTH = 140;
+
+$(() => { // shorthand for $(document).ready(() => {})
+  const $txtArea = $('section.new-tweet textarea');
+  $txtArea.on('input', function() {
+    const txt = $(this).val();
+    const val = TWEET_LENGTH - txt.length;
+    const $charCount = $(this).next().children().last();
+    if ( val < 0 ) {
+      $charCount.addClass('invalid');
+    } else {
+      $charCount.removeClass('invalid')
+    }
+    $charCount.val(val);
+  });
+});
+ 
