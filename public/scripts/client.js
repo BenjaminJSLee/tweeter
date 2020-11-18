8e5ded43b1854e31f5558d6f714392f60a24ece8
Benjamin Lee <benjaminjslee@gmail.com>
2020-11-18 22:15:04 +0000

    add errors for invalid form submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,40 +4,24 @@
 * Reminder: Use (and do all your DOM work in) jQuery's document ready function
 */
 
-// Test Driver code
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
- $(() => {
-
+$(() => {
+  
+  // Event listener for creating a new tweet
   $('.new-tweet form').submit(function(event) {
     event.preventDefault();
+    const MAX_TWEET_LENGTH = 140;
+    const $textarea = $('.new-tweet textarea');
+    if ($textarea.val() === "") {
+      alert("Error: cannot tweet an empty message");
+      return;
+    } else if ($textarea.val().length > MAX_TWEET_LENGTH) {
+      alert("Error: tweet has exceeded its maximum character length")
+      return;
+    }
     const formData = $( this ).serialize();
     $.ajax('/tweets', {type: "post", data: formData})
       .then((data) => {
-        
+        //loadTweets();
       })
       .catch(() => {
 
@@ -77,6 +61,16 @@ const data = [
     }
   };
 
-  renderTweets(data,$('.tweets'));
+  const loadTweets = () => {
+    $.ajax('/tweets', {type: 'get'})
+      .then((data) => {
+        renderTweets(data,$('.tweets'));
+      })
+      .catch((data) => {
 
- });
+      });
+  };
+
+  loadTweets();
+
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,9 +2,9 @@
 $(() => { // shorthand for $(document).ready(() => {})
 const $txtArea = $('section.new-tweet textarea');
 $txtArea.on('input', function() {
-    const TWEET_LENGTH = 140;
+    const MAX_TWEET_LENGTH = 140;
     const txt = $(this).val();
-    const val = TWEET_LENGTH - txt.length;
+    const val = MAX_TWEET_LENGTH - txt.length;
     const $charCount = $(this).next().children().last();
     if ( val < 0 ) {
       $charCount.addClass('invalid');
